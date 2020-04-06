30abbae950355be0c20d01a6c9f41c6464a7e32d
Marek <marek.skrzypkowski98@gmail.com>
2020-04-06 10:56:47 +0200

    .

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+public class App
+{
+    private Service service;
+    
+    public void setService(Service service)
+    {
+        this.service = service;
+    }
+    
+    public double pow(double a, double b)
+    {
+        return service.pow(a, b);
+    }
+
+    public double factorial(double a)
+    {
+        return service.factorial(a);
+    }
+    
+    public double sqrt(double a)
+    {
+        return service.sqrt(a);
+    }
+}
---
+++ src/main/java/Service.java
@@ -0,0 +1,6 @@
+public interface Service
+{
+    public double pow(double a, double b);
+    public double factorial(double a);
+    public double sqrt(double a);
+}
